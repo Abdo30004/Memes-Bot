ad9dc8c9993304bfe83728ce63b41b89d4848430
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2021-06-29 18:54:19 +0100

    Update help.js

--- commands/public/help.js
+++ commands/public/help.js
@@ -11,7 +11,7 @@ module.exports = {
     if (prefixdata === null) {
       prefix = client.config.prefix;
     } else {
-      prefix = prefixdata.Prefix;
+      prefix = prefixdata.Prefix.split("").map(item=>`\\${item}`).join("");
     }
     const devs = client.config.devs
     if (!args[0]) {
