a72d55d6ca6900414dae2b1c487c94ca2e7e6571
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2021-07-17 12:39:11 +0100

    Create blur.js

---
+++ commands/filters/blur.js
@@ -0,0 +1,27 @@
+const DIG = require("discord-image-generation");
+const Discord = require('discord.js');
+module.exports = {
+  async execute(client, message, args) {
+    if (!message.guild.me.hasPermission("ATTACH_FILES")) return message.channel.send("لا املك الصلاحيات الكافية")
+    let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member
+    
+    let avatar =await user.user.displayAvatarURL({ format: "png", size: 2048 });
+    let photo=message.attachments.first()?.proxyURL
+    var image=photo;
+    if(!photo){
+    image=avatar;
+    }
+     var img = await new DIG.Blur().getImage(image,10);
+    let attach = new Discord.MessageAttachment(img, `${user.user.username} blur.png`);
+    await message.channel.send(attach);
+
+
+  },
+};
+module.exports.help = {
+  name: 'blur',
+  aliases: ["تغبيش"],
+  category: 'filters',
+  description: "لتغبيش أفتارك وجعله غير واضح",
+  cooldown: 1,
+}
